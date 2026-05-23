58fb49b4d53b61ff3c0b3f895af93f2fc4b06387
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-05-23 20:44:14 +0300

    chore: init

---
+++ src/main/java/dev/dzyuba/albumtracker/AlbumTrackerApplication.java
@@ -0,0 +1,11 @@
+package dev.dzyuba.albumtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/dzyuba/albumtracker/AlbumTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dzyuba.albumtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
