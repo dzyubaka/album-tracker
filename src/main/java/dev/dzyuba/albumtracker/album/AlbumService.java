94a1c70a467278d2d04abecc29e602f8a7b7d277
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-05-27 17:35:29 +0300

    feat: album creation

--- src/main/java/dev/dzyuba/albumtracker/album/AlbumService.java
+++ src/main/java/dev/dzyuba/albumtracker/album/AlbumService.java
@@ -1,5 +1,6 @@
 package dev.dzyuba.albumtracker.album;
 
+import dev.dzyuba.albumtracker.band.BandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,17 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AlbumService {
+    private final BandRepository bandRepository;
+
     private final AlbumRepository albumRepository;
 
     public List<AlbumResponse> findByBandId(Long bandId) {
         return albumRepository.findByBandId(bandId).stream().map(a -> new AlbumResponse(a.getId(), a.getName())).toList();
+    }
+
+    public AlbumResponse save(Long bandId, Album album) {
+        album.setBand(bandRepository.findById(bandId).orElseThrow());
+        album = albumRepository.save(album);
+        return new AlbumResponse(album.getId(), album.getName());
     }
 }
--- src/main/java/dev/dzyuba/albumtracker/band/BandController.java
+++ src/main/java/dev/dzyuba/albumtracker/band/BandController.java
@@ -1,12 +1,10 @@
 package dev.dzyuba.albumtracker.band;
 
+import dev.dzyuba.albumtracker.album.Album;
 import dev.dzyuba.albumtracker.album.AlbumResponse;
 import dev.dzyuba.albumtracker.album.AlbumService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class BandController {
     @GetMapping("/{id}/albums")
     public List<AlbumResponse> albums(@PathVariable Long id) {
         return albumService.findByBandId(id);
+    }
+
+    @PostMapping("/{id}/albums")
+    public AlbumResponse albums(@PathVariable Long id, @RequestBody Album album) {
+        return albumService.save(id, album);
     }
 }
