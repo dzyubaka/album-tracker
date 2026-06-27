d467ddbebaf3613ca0f2a4fe17ffa413e152fed0
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-06-27 11:30:40 +0300

    feat: order bands by name

--- src/main/java/dev/dzyuba/albumtracker/band/BandService.java
+++ src/main/java/dev/dzyuba/albumtracker/band/BandService.java
@@ -1,6 +1,7 @@
 package dev.dzyuba.albumtracker.band;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public class BandService {
     private final BandRepository bandRepository;
 
     public List<BandResponse> findAll() {
-        return bandRepository.findAll().stream().map(b -> new BandResponse(b.getId(), b.getName())).toList();
+        return bandRepository.findAll(Sort.by("name")).stream().map(b -> new BandResponse(b.getId(), b.getName())).toList();
     }
 
     public BandResponse save(Band band) {
